c7b9d1fbbd9fe1bbe7cb6d6c683b8aef037752cd
Jessica <jessi@schifferweb.de>
2021-03-02 10:37:54 +0100

    TestErneutHTML

--- Index.php
+++ Index.php
@@ -1,5 +1,11 @@
-Hallo erneut
-<br>
-Hallo erneut2
-<br>
-Hallo erneut3
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Versuch1</title>
+</head>
+<body>
+<h1>Überschrift1</h1>
+
+</body>
+</html>
