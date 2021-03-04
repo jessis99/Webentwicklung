49228a05e546347d6911362e9031a2bd0440adcc
Jessica <jessi@schifferweb.de>
2021-03-04 17:18:05 +0100

    PHP neu

--- Index.php
+++ Index.php
@@ -4,18 +4,51 @@
     <meta charset="UTF-8">
     <title>Jessis zweite Seite</title>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="stylesPHP.css"/>
 </head>
 <body>
 <div class="container">
+    <div  class="jumbotron" id="Ueberschrift">
+        <h1><p class="text-primary">Schiffers</p></h1>
+    </div>
     <div class="row">
         <div class="col">
-            first
+            <div class="jumbotron" id="Bianca">
+                Bianca
+            </div>
         </div>
         <div class="col">
-            secound
+            <div class="jumbotron" id="Dirk">
+                Dirk
+            </div>
         </div>
         <div class="col">
-            third
+            <div class="jumbotron" id="Jessica">
+                Jessi
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron" id="Melina">
+                Melina
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron" id="Niclas">
+                Niclas
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="jumbotron" id="Brunhild">
+                Brunhild
+            </div>
+        </div>
+        <div class="col">
+            <div class="jumbotron" id="Matthias">
+                Matthias
+            </div>
         </div>
     </div>
 </div>
