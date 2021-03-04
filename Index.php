ebd530ce794d1cecd66705b5e5be90abbf1fae6e
Jessica <jessi@schifferweb.de>
2021-03-04 14:17:08 +0100

    PHP

---
+++ Index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Jessis zweite Seite</title>
+    <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"/>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            first
+        </div>
+        <div class="col">
+            secound
+        </div>
+        <div class="col">
+            third
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
