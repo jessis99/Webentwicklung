3b48b8c0734b76cc51112a2cf014d9a08993c2f4
Jessica <jessi@schifferweb.de>
2021-03-08 14:37:13 +0100

    PHP

--- Index.php
+++ Index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <title>Jessis zweite Seite</title>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@4.5.3/dist/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="stylesPHP.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+    <link rel="stylesheet" href="stylesPHP.css"/>
     <script src="https://unpkg.com/jquery@3.5.1/dist/jquery.min.js"></script>
     <script src="https://unpkg.com/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
